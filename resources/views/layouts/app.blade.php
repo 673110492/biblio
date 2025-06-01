a4313497c3f937f96b6b67c302fa569f2529ecc3
673110492 <rincedonfack5@gmail.com>
2025-06-01 15:17:29 +0100

    modif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -153,7 +153,20 @@
                     <div class="container-fluid">
 
 
-                        
+                        <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
+                            <li class="nav-item topbar-icon dropdown hidden-caret d-flex d-lg-none">
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#"
+                                    role="button" aria-expanded="false" aria-haspopup="true">
+                                    <i class="fa fa-search"></i>
+                                </a>
+                                <ul class="dropdown-menu dropdown-search animated fadeIn">
+                                    <form class="navbar-left navbar-form nav-search">
+                                        <div class="input-group">
+                                            <input type="text" placeholder="Search ..." class="form-control" />
+                                        </div>
+                                    </form>
+                                </ul>
+                            </li>
                             <li class="nav-item topbar-icon dropdown hidden-caret">
                                 <a class="nav-link dropdown-toggle" href="#" id="messageDropdown"
                                     role="button" data-bs-toggle="dropdown" aria-haspopup="true"
